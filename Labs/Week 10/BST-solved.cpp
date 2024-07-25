b2c8bfed01bee27c0bf411abab12fa050aabe823
hassaan141 <hassaanfarooqi2000@gmail.com>
2024-07-25 19:53:40 -0400

    new changes

---
+++ Labs/Week 10/BST-solved.cpp
@@ -0,0 +1,177 @@
+/*
+Written for MTE 140 by Harish Prakash (TA). 
+Topic: Binary Search Tree Operations
+
+Solutions for Lab 10. 
+I haven't coded the bonus question (vertical print), so feel free to do it yourself/refer online!
+*/
+
+#include <iostream>
+#include <queue>
+#include <iomanip>
+using namespace std;
+
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        key = value;
+        left = right = nullptr;
+    }
+};
+
+class BST {
+public:
+    Node* root;
+
+    BST() {
+        root = nullptr;
+    }
+
+    void insert(int key) {
+        root = insertRecursive(root, key);
+    }
+
+    void deleteKey(int key) {
+        root = deleteRecursive(root, key);
+    }
+
+    Node* search(int key) {
+        return searchRec(root, key);
+    }
+
+    void printlevelorder() {
+        if (root == nullptr) return;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            Node* current = q.front();
+            q.pop();
+            cout << current->key << " ";
+
+            if (current->left != nullptr) q.push(current->left);
+            if (current->right != nullptr) q.push(current->right);
+        }
+        cout << endl;
+    }
+
+    void printTree() {
+        printTreeRec(root, 0);
+    }
+
+
+private:
+    Node* insertRecursive(Node* node, int key) {
+        if (node == nullptr) {
+            return new Node(key);
+        }
+
+        if (key < node->key) {
+            node->left = insertRecursive(node->left, key);
+        } else if (key > node->key) {
+            node->right = insertRecursive(node->right, key);
+        }
+        return node;
+    }
+
+    Node* deleteRecursive(Node* root, int key) {
+
+        if (root == nullptr) return root;
+        if (key < root->key) {
+            root->left = deleteRecursive(root->left, key);
+        } else if (key > root->key) {
+            root->right = deleteRecursive(root->right, key);
+
+        } else {
+            if (root->left == nullptr) {
+                Node* temp = root->right;
+                delete root;
+                return temp;
+
+            } else if (root->right == nullptr) {
+                Node* temp = root->left;
+                delete root;
+                return temp;
+            }
+            Node* temp = minValueNode(root->right);
+            root->key = temp->key;
+            root->right = deleteRecursive(root->right, temp->key);
+        }
+        return root;
+    }
+
+    Node* searchRec(Node* node, int key) {
+        if (node == nullptr || node->key == key) {
+            return node;
+        }
+
+        if (key < node->key) {
+            return searchRec(node->left, key);
+        }
+
+        return searchRec(node->right, key);
+    }
+
+    Node* minValueNode(Node* node) {
+        Node* current = node;
+        while (current && current->left != nullptr)
+            current = current->left;
+        return current;
+    }
+
+    void printTreeRec(Node* node, int space) {
+        if (node == nullptr)
+            return;
+
+        space += 10;
+
+        printTreeRec(node->right, space);
+
+        cout << endl;
+        for (int i = 10; i < space; i++)
+            cout << " ";
+        cout << node->key << "\n";
+
+        printTreeRec(node->left, space);
+    }
+};
+
+int main() {
+    BST bst;
+    bst.insert(50);
+    bst.insert(30);
+    //bst.insert(20);
+    bst.insert(40);
+    bst.insert(70);
+    bst.insert(60);
+    bst.insert(80);
+
+    cout << "Tree structure:" << endl;
+    bst.printTree();
+
+    cout << "\nDeleting 20 ..." << endl;
+    bst.deleteKey(20);
+    bst.printTree();
+
+    cout << "\nDeleting 30 ..." << endl;
+    bst.deleteKey(30);
+    bst.printTree();
+
+    cout << "\nDeleting 50 ..." << endl;
+    bst.deleteKey(50);
+    bst.printTree();
+
+    int key = 40;
+    cout<<"\nSearching for "<<key <<" ...";
+    if (bst.search(key) != nullptr) {
+        cout << "\nFound " << key << endl;
+    } else {
+        cout << key << " not found" << endl;
+    }
+
+    return 0;
+}
---
+++ Labs/Week 10/week10MaxHeap.cpp
@@ -0,0 +1,259 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class HeapNode{
+private:
+    int key;                              // Heap item priority key
+    string data;                         // data value
+
+public:
+    HeapNode();
+    HeapNode(int key, string data);     // Constructor
+    int getKey();                         // Return item priority
+    void setKey(int key);               // Set the priority key value
+    string getData();                    // Return data item
+    void setData(string data);          // Set the data item value
+};
+
+//-----------------------------------
+// Constructor
+//-----------------------------------
+HeapNode::HeapNode(): key(0), data(""){ }
+
+HeapNode::HeapNode(int k, string str): key(k), data(str){ }
+
+
+//-----------------------------------
+// Return item priority
+//-----------------------------------
+int HeapNode::getKey(){
+    return key;
+}
+
+//-----------------------------------
+// Set the priority key value
+//-----------------------------------
+void HeapNode::setKey(int key){
+    key = key;
+}
+
+//-----------------------------------
+// Return data item
+//-----------------------------------
+string HeapNode::getData(){
+    return data;
+}
+
+//-----------------------------------
+// Set the data item value
+//-----------------------------------
+void HeapNode::setData(string str){
+    data = str;
+}
+
+
+
+class Heap{
+private:
+    HeapNode     *elements;                 // Pointer to dynamically allocated array
+    int          size;              // Number of elements in the heap
+    int          capacity;
+
+public:
+    Heap(int cap);
+    ~Heap();                                 // Destructor
+
+    bool Enqueue(HeapNode item);
+    void ReheapUp(int index);     // Reheap after inserting item
+
+    HeapNode Dequeue();                     // Get item at the root
+    void ReheapDown(int index);   // Reheap after removing item
+
+    int getSize();                    // Return number of elements in the heap
+    void printAll();                         // Print all the elements in the heap
+};
+
+//---------------------------------------
+// constructor
+//---------------------------------------
+Heap::Heap(int cap){
+    elements = new HeapNode[cap];
+    size = 0;
+    capacity = cap;
+}
+
+//---------------------------------------
+// Destructor
+//---------------------------------------
+Heap::~Heap(){
+    delete[] elements;
+}
+
+//---------------------------------------
+// Add an item to the heap
+//---------------------------------------
+bool Heap::Enqueue(HeapNode item){
+    if(size == capacity) return false;
+    else {
+        elements[size + 1] = item; // Copy item into array
+        ReheapUp(size + 1);
+        size++;
+        return true;
+    }
+}
+
+
+
+//---------------------------------------
+// Reheap after inserting item
+//---------------------------------------
+void Heap::ReheapUp(int index){
+    ////////////////////////////////////////////////////////////////////
+
+    //Until reaching the root node (no more parent)
+    if(index == 1) return;
+
+    //	If the parent has a smaller value than the node, swap with the parent
+    int parent = index / 2;
+    if(elements[parent].getKey() < elements[index].getKey()){
+        // Swap these two elements
+        HeapNode temp = elements[parent];
+        elements[parent] = elements[index];
+        elements[index] = temp;
+        // Make recursive call till reheaping completed
+        ReheapUp(parent);
+    }
+    else return;
+
+    ////////////////////////////////////////////////////////////////////
+
+}
+
+
+//---------------------------------------
+// Get item at the root
+//---------------------------------------
+HeapNode Heap::Dequeue(){
+    if(size == 0)  return HeapNode(-9999, "EMPTY_HEAP_RETURN");  // special case
+
+    //index 1 is the root. Use temp to save the to-be-returned node.
+    HeapNode temp = HeapNode(elements[1]);  //default copy constructor, shallow copy
+
+    // Copy last item into root
+    elements[1] = elements[size]; //default assignment copy
+
+    size--;
+
+    // Reheap the tree
+    ReheapDown(1);  //from root node, index 1
+
+    return temp;
+}
+
+//---------------------------------------
+// Reheap after removing item
+//---------------------------------------
+void Heap::ReheapDown(int index){
+    ////////////////////////////////////////////////////////////////////
+    //Exit when reaching a leaf node (the bottom)
+    if (2 * index > size) return;
+
+    //	If there is any child of the node at index,
+    int chosenChild;
+
+    if (2 * index == size) chosenChild = size;  // only one child, it must be the left child
+    else { // if two children, find the larger child. If same, e.g., prefer left.
+        int leftChild = 2 * index;
+        int rightChild = 2 * index + 1;
+        if( elements[leftChild].getKey() >= elements[rightChild].getKey() ) chosenChild = leftChild;
+        else chosenChild = rightChild;
+    }
+
+
+    if(elements[chosenChild].getKey() > elements[index].getKey()){
+        //If the child has a key larger than the node, swap the node with that child.
+        HeapNode temp = elements[chosenChild];
+        elements[chosenChild] = elements[index];
+        elements[index] = temp;
+
+        ReheapDown(chosenChild); //Continue re-heap downwards
+    }
+    else return;
+
+    ////////////////////////////////////////////////////////////////////
+
+}
+
+
+
+
+//---------------------------------------
+// Return number of elements in the heap
+//---------------------------------------
+int Heap::getSize(){
+    return size;
+}
+
+//---------------------------------------
+// Print all the elements in the heap
+//---------------------------------------
+void Heap::printAll(){
+    if(size == 0) cout<< "Empty!" <<endl;
+    for(int i=1; i<=size; i++){
+        cout << "Heap element " << i << ". key = " << elements[i].getKey() << ",  data = " << elements[i].getData() << endl;
+    }
+}
+
+//main
+int main(){
+
+    Heap *theHeap = new Heap(20);  // Create a heap of the default size
+
+    HeapNode obj_a = HeapNode(10, "a");
+    theHeap->Enqueue( obj_a );
+    theHeap->Enqueue( HeapNode(5, "b"));
+    theHeap->Enqueue( HeapNode(7, "c"));
+    theHeap->Enqueue( HeapNode(4, "d"));
+    theHeap->Enqueue( HeapNode(2, "e"));
+    theHeap->Enqueue( HeapNode(1, "f"));
+    theHeap->printAll(); //this is breadth first, level traversal
+
+
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+	cout<<"enqueue 7 (g) " << endl;
+	theHeap->Enqueue( HeapNode(7, "g"));
+    theHeap->printAll(); //this is breadth first, level traversal
+
+	cout<<"enqueue 5 (h) " << endl;
+	theHeap->Enqueue( HeapNode(5, "h"));
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+    cout<<"dequeue data: " <<  theHeap->Dequeue().getData() << endl;
+    theHeap->printAll(); //this is breadth first, level traversal
+
+
+    return 0;
+}
